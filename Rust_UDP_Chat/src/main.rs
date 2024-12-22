8f4adc0bf0b900cfbd9a172117185537adedef6a
feifei <1727906692@qq.com>
2024-12-23 02:02:53 +0800

    修复了可以重名的bug，但是出现了有关名称的一系列问题，明天再修

--- Rust_UDP_Chat/src/main.rs
+++ Rust_UDP_Chat/src/main.rs
@@ -1,60 +1,120 @@
 use futures_util::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
-};
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use tokio::sync::broadcast;
 use warp::Filter;
 
-// 为 Message 结构体添加序列化和反序列化的派生宏
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct Message {
-    user_name: String,
-    content: String,
-    timestamp: String,
+#[serde(tag = "type")]
+enum ChatMessage {
+    Init {
+        user_name: String,
+        timestamp: String,
+    },
+    Chat {
+        user_name: String,
+        content: String,
+        timestamp: String,
+    },
+    System {
+        content: String,
+        timestamp: String,
+    },
+    Error {
+        content: String,
+    }
 }
 
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 #[tokio::main]
 async fn main() {
-    // 创建广播通道
     let (tx, _rx) = broadcast::channel(100);
     let tx = Arc::new(tx);
+    let usernames = Arc::new(Mutex::new(HashSet::new()));
 
-    // 静态文件路由
     let static_files = warp::path("static").and(warp::fs::dir("static"));
     let index = warp::path::end().and(warp::fs::file("static/index.html"));
 
-    // WebSocket 路由
+    let usernames = warp::any().map(move || usernames.clone());
     let tx_filter = warp::any().map(move || tx.clone());
+    
     let ws_route = warp::path("ws")
         .and(warp::ws())
         .and(tx_filter)
-        .map(|ws: warp::ws::Ws, tx: Arc<broadcast::Sender<Message>>| {
-            ws.on_upgrade(move |socket| handle_client(socket, tx))
+        .and(usernames)
+        .map(|ws: warp::ws::Ws, tx: Arc<broadcast::Sender<ChatMessage>>, usernames: Arc<Mutex<HashSet<String>>>| {
+            ws.on_upgrade(move |socket| handle_client(socket, tx, usernames))
         });
 
-    // 合并所有路由
     let routes = static_files.or(index).or(ws_route);
 
     println!("Server started at http://127.0.0.1:8080");
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
 
-async fn handle_client(websocket: warp::ws::WebSocket, tx: Arc<broadcast::Sender<Message>>) {
+async fn handle_client(
+    websocket: warp::ws::WebSocket,
+    tx: Arc<broadcast::Sender<ChatMessage>>,
+    usernames: Arc<Mutex<HashSet<String>>>,
+) {
     let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
-    println!("New client connected: {}", my_id);
-
     let (mut client_ws_sender, mut client_ws_rcv) = websocket.split();
     let mut rx = tx.subscribe();
+    let mut user_name = String::new();
 
-    // 发送消息的任务
+    // 处理连接
+    while let Some(result) = client_ws_rcv.next().await {
+        if let Ok(msg) = result {
+            if let Ok(text) = msg.to_str() {
+                if let Ok(message) = serde_json::from_str::<ChatMessage>(text) {
+                    match message {
+                        ChatMessage::Init { user_name: name, timestamp } => {
+                            // 检查用户名是否可用
+                            let is_username_taken = {
+                                let usernames = usernames.lock().unwrap();
+                                usernames.contains(&name)
+                            }; // MutexGuard在这里被释放
+
+                            if is_username_taken {
+                                let error = ChatMessage::Error {
+                                    content: "该用户名已被使用，请选择其他用户名".to_string(),
+                                };
+                                let _ = client_ws_sender
+                                    .send(warp::ws::Message::text(serde_json::to_string(&error).unwrap()))
+                                    .await;
+                                continue;
+                            }
+
+                            // 添加新用户名
+                            {
+                                let mut usernames = usernames.lock().unwrap();
+                                user_name = name.clone();
+                                usernames.insert(name);
+                            } // MutexGuard在这里被释放
+
+                            // 广播用户加入消息
+                            let join_msg = ChatMessage::System {
+                                content: format!("{} 加入了聊天室", user_name),
+                                timestamp,
+                            };
+                            let _ = tx.send(join_msg);
+                            break;
+                        }
+                        _ => continue,
+                    }
+                }
+            }
+        }
+    }
+
+    // 处理消息
     let mut send_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
             let msg_str = serde_json::to_string(&msg).unwrap_or_default();
-            if let Err(_disconnected) = client_ws_sender
+            if let Err(_) = client_ws_sender
                 .send(warp::ws::Message::text(msg_str))
                 .await
             {
@@ -63,19 +123,12 @@ async fn handle_client(websocket: warp::ws::WebSocket, tx: Arc<broadcast::Sender
         }
     });
 
-    // 接收消息的任务
     let tx_clone = tx.clone();
     let mut recv_task = tokio::spawn(async move {
-        while let Some(result) = client_ws_rcv.next().await {
-            let msg = match result {
-                Ok(msg) => msg,
-                Err(_) => break,
-            };
-
+        while let Some(Ok(msg)) = client_ws_rcv.next().await {
             if let Ok(text) = msg.to_str() {
-                let msg_result: Result<Message, _> = serde_json::from_str(text);
-                if let Ok(chat_msg) = msg_result {
-                    let _ = tx_clone.send(chat_msg);
+                if let Ok(message) = serde_json::from_str::<ChatMessage>(text) {
+                    let _ = tx_clone.send(message);
                 }
             }
         }
@@ -85,6 +138,20 @@ async fn handle_client(websocket: warp::ws::WebSocket, tx: Arc<broadcast::Sender
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
     };
+
+    // 清理用户名和发送离开消息
+    if !user_name.is_empty() {
+        {
+            let mut usernames = usernames.lock().unwrap();
+            usernames.remove(&user_name);
+        } // MutexGuard在这里被释放
+
+        let leave_msg = ChatMessage::System {
+            content: format!("{} 离开了聊天室", user_name),
+            timestamp: chrono::Local::now().format("%H:%M:%S").to_string(),
+        };
+        let _ = tx.send(leave_msg);
+    }
 
     println!("Client disconnected: {}", my_id);
 }
